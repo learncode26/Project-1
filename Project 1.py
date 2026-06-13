118fad8d4b7ab087935dab47ec0e673ac7e6d5b0
Ayra <aimlcourse25@gmail.com>
2026-06-13 12:16:36 +0100

    initial commit

---
+++ Project 1.py
@@ -0,0 +1,19 @@
+print("My Daily Routine!")
+
+# Wake up
+print("I wake up at 7:30am.")
+
+# Go to school
+print("I go to school at 8:15am.")
+
+# Coming home
+print("I come back home at 3:30pm.")
+
+# Studying
+print("I study for 1hr every day.")
+
+# Sleep
+print("I sleep at 10:30pm.")
+
+# Total study time
+print("Total study time in a week(in hours):" , 1 * 7)

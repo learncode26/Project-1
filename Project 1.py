9e0c1bd6c9600cff3793ca7ebebef966600a8af3
Ayra <aimlcourse25@gmail.com>
2026-06-13 17:25:50 +0100

    Class 2

--- Project 1.py
+++ Project 1.py
@@ -1,19 +1,20 @@
-print("My Daily Routine!")
+print("My Daily Routine!🕐")
 
 # Wake up
-print("I wake up at 7:30am.")
+print("I wake up at 7:30am.⏰")
 
 # Go to school
-print("I go to school at 8:15am.")
+print("I go to school at 8:15am.🏫")
 
 # Coming home
-print("I come back home at 3:30pm.")
+print("I come back home at 3:30pm.🏠")
 
 # Studying
-print("I study for 1hr every day.")
+print("I study for 1hr every day.📖")
 
 # Sleep
-print("I sleep at 10:30pm.")
+print("I sleep at 10:30pm.🛏️")
 
 # Total study time
-print("Total study time in a week(in hours):" , 1 * 7)
+print("📝 Total study time in a week(in hours):" , 1 * 7)
+
